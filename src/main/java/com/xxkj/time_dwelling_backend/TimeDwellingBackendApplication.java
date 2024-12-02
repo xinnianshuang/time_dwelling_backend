6d9f008dc1b17d176948d4fe9f79fcd72b351ccb
xxin <2512325502@qq.com>
2024-12-02 10:50:55 +0800

    first commit

---
+++ src/main/java/com/xxkj/time_dwelling_backend/TimeDwellingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xxkj.time_dwelling_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDwellingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDwellingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xxkj/time_dwelling_backend/TimeDwellingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxkj.time_dwelling_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDwellingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
